38406f790e246c7d05121e45516d6290c3f4c67a
Prashant Malviya <prashantmalviya272002@gmail.com>
2024-09-07 03:03:17 +0530

    dropdown menu for small and medium devices added

--- src/components/TimeSelector.jsx
+++ src/components/TimeSelector.jsx
@@ -5,20 +5,38 @@ const TimeSelector = ({ onSelectTime }) => {
 
   const handleTimeChange = (time) => {
     setSelectedTime(time);
-    onSelectTime(time); // Notify parent component of the time change
+    onSelectTime(time); 
   };
 
   return (
-    <div className="space-x-2">
-      {['1 Day', '1 Week', '1 Month', '1 Year'].map((time) => (
-        <button
-          key={time}
-          className={`py-2 px-4 rounded-lg ${selectedTime === time ? 'bg-blue-600 text-white' : 'bg-gray-200'}`}
-          onClick={() => handleTimeChange(time)}
+    <div>
+      {/* Dropdown for small and medium screens */}
+      <div className="block lg:hidden">
+        <select
+          value={selectedTime}
+          onChange={(e) => handleTimeChange(e.target.value)}
+          className="py-2 px-4 rounded-lg bg-gray-200 w-full"
         >
-          {time}
-        </button>
-      ))}
+          {['1 Day', '1 Week', '1 Month', '1 Year'].map((time) => (
+            <option key={time} value={time}>
+              {time}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {/* Buttons for large screens */}
+      <div className="hidden lg:flex space-x-2">
+        {['1 Day', '1 Week', '1 Month', '1 Year'].map((time) => (
+          <button
+            key={time}
+            className={`py-2 px-4 rounded-lg ${selectedTime === time ? 'bg-blue-600 text-white' : 'bg-gray-200'}`}
+            onClick={() => handleTimeChange(time)}
+          >
+            {time}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
